2c6c2dd8272a5721fb88deee5b02b02f6a8e8e1b
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-02-19 19:00:12 +0100

    creatig a hash table, adding, searching, adding, and removing items

--- implementations/hash_table.py
+++ implementations/hash_table.py
@@ -7,23 +7,28 @@ class HashTable:
         hash = 0
         for char in key:
             hash += ord(char)
-        return hash % self.MAX # Returns the hash value of the key
+        return hash % self.MAX # Returns the hash value of the key (big O(n) operation because we need to traverse the key)
     
     def __setitem__(self, key, val): # allows us to use the assignment operator to set the value of a key
         h = self.get_hash(key)
-        self.arr[h] = val # Overwrite the value if the key already exists, stores the value at the hash index
+        self.arr[h] = val # Overwrite the value if the key already exists, stores the value at the hash index, big O(1) operation
 
     def __getitem__(self, key): # allows us to use the index operator to get the value of a key
         h = self.get_hash(key)
-        return self.arr[h] # Returns the value at the hash index
+        return self.arr[h] # Returns the value at the hash index, big O(1) operation
     
-    
+    def del_item(self, key):
+        h = self.get_hash(key)
+        self.arr[h] = None # Deletes the value at the hash index, big O(1) operation
+        
+        
 t = HashTable()
 
 print(t.get_hash('march 12')) # 54
 print(t.get_hash('march 6')) # 9
 t['march 6'] = 130
 t['march 12'] = 140
+t.del_item('march 6')
 
 print(t['march 6']) # 130
 print(t['march 12']) # 140
