f66078be2bcb62b6d8567554da6fbf294f9bf41a
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-03-09 17:31:06 +0100

    updated bubble sort for efficiency when an array is sorted

--- implementations/Algorithms/bubble_sort.py
+++ implementations/Algorithms/bubble_sort.py
@@ -2,10 +2,15 @@
 
 def bubble_sort(arr):
     n = len(arr)
+    
+    swapped = False
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j] # swap the elements
+                swapped = True
+        if not swapped: # if no two elements were swapped in the inner loop, then the array is already sorted
+            break
     return arr
 
 if __name__ == '__main__':
