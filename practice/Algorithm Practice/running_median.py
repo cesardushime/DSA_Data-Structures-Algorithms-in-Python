bdb151f3b06ffaa4c35cb5ffec66ed6e3638a785
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-03-09 19:48:38 +0100

    computing running median by insertion sort logic

---
+++ practice/Algorithm Practice/running_median.py
@@ -0,0 +1,26 @@
+# Computing the running median of a sequence of numbers
+
+def running_median(sequence):
+    sorted_sequence = []
+    medians = []
+    
+    for index, element in enumerate(sequence):
+        sorted_sequence.append(element)
+        sorted_sequence.sort()
+        size = len(sorted_sequence)
+        
+        if size  % 2 == 1:
+            median_index = size // 2
+            median = sorted_sequence[median_index]
+        else:
+            right_index = size // 2
+            left_index = right_index -1 
+            
+            median = (sorted_sequence[left_index] + sorted_sequence[right_index]) / 2
+        
+        medians.append(median)
+    return medians
+        
+if __name__ == '__main__':
+    sequence = [2, 1, 5, 7, 2, 0, 5]
+    print(running_median(sequence)) # [2, 1.5, 2, 3.5, 2, 2, 2]
