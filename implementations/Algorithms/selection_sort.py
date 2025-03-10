1024e0efc89b59e72966cb72ce7edb8862fca6b5
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-03-10 11:17:48 +0100

    optiimized technique for selection sort

--- implementations/Algorithms/selection_sort.py
+++ implementations/Algorithms/selection_sort.py
@@ -1,12 +1,13 @@
 # Implementing selection sort algorithm
 
 def selection_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)-1): # because the last element will be at its correct position after n-1 iterations
         min_index = i
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_index]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        if i != min_index: # If the minimum element is not at the current index, swap the elements
+            arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
 if __name__=='__main__':
