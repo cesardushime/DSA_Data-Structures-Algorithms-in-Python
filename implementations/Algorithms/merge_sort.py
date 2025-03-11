8f4eb089ef8e9734592ad3c471d100b01f973e47
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-03-11 10:22:29 +0100

    implementing mergeSort full functionality to sort an unsorted single array

--- implementations/Algorithms/merge_sort.py
+++ implementations/Algorithms/merge_sort.py
@@ -1,5 +1,17 @@
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2 # floor division round down to the nearest whole number
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = mergeSort(left) # recursively divide the left half of the array until we have single elements
+    right = mergeSort(right) # recursively divide the right half of the array until we have single elements
+
+    return merge2_sortedArrays(left, right) # repeatedly merge the sorted arrays until we have a single sorted array
+
 def merge2_sortedArrays(a,b):
-    sort_list = []
+    sorted_list = []
     
     len_a = len(a)
     len_b = len(b)
@@ -7,23 +19,23 @@ def merge2_sortedArrays(a,b):
     
     while i < len_a and j < len_b:
         if a[i] <= b[j]:
-            sort_list.append(a[i])
+            sorted_list.append(a[i])
             i += 1
         else:
-            sort_list.append(b[j])
+            sorted_list.append(b[j])
             j += 1
     # handle the remaining elements when one of the arrays is exhausted
     while i < len_a:
-        sort_list.append(a[i])
+        sorted_list.append(a[i])
         i += 1
     while j < len_b:
-        sort_list.append(b[j])
+        sorted_list.append(b[j])
         j += 1      
-    return sort_list
+    return sorted_list
     
 
 if __name__ == '__main__':
-    a = [1, 3, 5, 7]
-    b = [2, 4, 6, 8, 10, 15]
+    # Unsorted array
+    arr = [3, 7, 1, 8, 2, 4, 5, 6, 10, 0]
     
-    print(merge2_sortedArrays(a,b)) # [1, 2, 3, 4, 5, 6, 7, 8]
+    print(mergeSort(arr)) # [1, 2, 3, 4, 5, 6, 7, 8]
