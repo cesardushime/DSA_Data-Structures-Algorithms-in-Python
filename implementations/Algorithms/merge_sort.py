765bb883508bca8f5e1db9165e45b8c59df6e19f
cesardushime <125463453+cesardushime@users.noreply.github.com>
2025-03-11 10:06:23 +0100

    handle remaining elements when one of the arrays is exhausted

--- implementations/Algorithms/merge_sort.py
+++ implementations/Algorithms/merge_sort.py
@@ -12,7 +12,13 @@ def merge2_sortedArrays(a,b):
         else:
             sort_list.append(b[j])
             j += 1
-            
+    # handle the remaining elements when one of the arrays is exhausted
+    while i < len_a:
+        sort_list.append(a[i])
+        i += 1
+    while j < len_b:
+        sort_list.append(b[j])
+        j += 1      
     return sort_list
     
 
@@ -20,4 +26,4 @@ if __name__ == '__main__':
     a = [1, 3, 5, 7]
     b = [2, 4, 6, 8, 10, 15]
     
-    print(mergeSort(a,b)) # [1, 2, 3, 4, 5, 6, 7, 8]
+    print(merge2_sortedArrays(a,b)) # [1, 2, 3, 4, 5, 6, 7, 8]
