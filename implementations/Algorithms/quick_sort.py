44fc51b2f1727139dc486ffe56fc172b73a5e956
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-12 17:12:30 +0100

    quick sort complete implementation

--- implementations/Algorithms/quick_sort.py
+++ implementations/Algorithms/quick_sort.py
@@ -1,43 +1,42 @@
 # Implementation of quick sort algorithm
-
 def quick_sort(arr, left, right):
     if left < right:
-        partition_index = partition(arr, left, right)
-        quick_sort(arr, left, partition_index - 1)
-        quick_sort(arr, partition_index + 1, right)
-    
-    
+        partition_index = partition(arr, left, right)  # Get partition index
+        quick_sort(arr, left, partition_index - 1) 
+        quick_sort(arr, partition_index + 1, right)  
 
 def partition(arr, left, right):
-    i = left
-    j = right - 1
-    pivot = arr[right]
+    pivot = arr[right]  
+    i = left  # i starts at the leftmost element
+    j = right - 1  # j starts at the element before the pivot
     
-    while i <= j:
-        while i < j and arr[i] < pivot:
+    while i <= j:  # The loop continues until i surpasses j
+        while i <= j and arr[i] < pivot:  # i moves to the right to find elements >= pivot
             i += 1
-        while i < j and arr[j] >= pivot:
-            j -= 1 
+        while i <= j and arr[j] > pivot:  # j moves to the left to find elements <= pivot
+            j -= 1
         
-        # swap the elements at i and j if elements at i is greater than pivot and elements at j is less than pivot
-        if i < j:
+        if i <= j:  # Swap arr[i] and arr[j] if i has not surpassed j
             arr[i], arr[j] = arr[j], arr[i]
-            
-    # swap the pivot with the element at i
-    if arr[i] > pivot:
-        arr[i], arr[right] = arr[right], arr[i]
-    return i # return the partition index
+            i += 1
+            j -= 1
+    
+    # Place the pivot in its correct position as i has surpassed j
+    arr[i], arr[right] = arr[right], arr[i]
+    return i  # Return the partition index
 
 
 if __name__ == '__main__':
-    # Testing with mixed and nested lists
-    testCases = [
-        [1, 2, 3, 4, 5, 6, 7, 8],
-        [8, 7, 6, 5, 4, 3, 2, 1],
-        [1, 3, 5, 7, 2, 4, 6, 8],
-        [],
+    # 5 List variety of test cases
+    test_cases = [
+        [1, 2, 3, 4, 5],
+        [5, 4, 3, 2, 1],
+        [1, 3, 5, 2, 4],
+        [5, 3, 1, 4, 2],
+        [5, 3, 1, 2, 4]
     ]
-    
-    for arr in testCases:
+
+    for arr in test_cases:
+        print(f"Before: {arr}")
         quick_sort(arr, 0, len(arr) - 1)
-        print(arr) # [1, 2, 3, 4, 5, 6, 7, 8] [1, 2, 3, 4, 5, 6, 7, 8] [1, 2, 3, 4, 5, 6, 7, 8] []
+        print(f"After: {arr}\n")
