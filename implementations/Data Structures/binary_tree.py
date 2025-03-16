5d4e9e9cb0ac0f46573ae22db3efdc2329f3c3bb
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-16 18:22:14 +0100

    binary search tree add child functionality

--- implementations/Data Structures/binary_tree.py
+++ implementations/Data Structures/binary_tree.py
@@ -8,3 +8,22 @@ class BinaryTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return 
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinaryTreeNode(data)
+            
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinaryTreeNode(data)
+
+    
