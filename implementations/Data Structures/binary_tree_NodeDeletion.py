1535d45d0c5556e327c2ee3b70d3e1433c42f93d
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-17 15:31:24 +0100

    delete method to delete an element from BST gracefully

--- implementations/Data Structures/binary_tree_NodeDeletion.py
+++ implementations/Data Structures/binary_tree_NodeDeletion.py
@@ -26,7 +26,14 @@ class BinaryTreeNode:
             else:
                 self.right = BinaryTreeNode(data)
 
-    def in_order_traversal(self):
+    def build_tree(elements):
+        root = BinaryTreeNode(elements[0])
+        for i in range(1, len(elements)):
+            root.add_child(elements[i]) # adds elements to the tree by making them children of the root node, while maintaining the binary search tree property
+        
+        return root
+
+    def in_order_traversal(self): # left -> base -> right, also known as depth-first search (DFS)
         elements = []
         # visit left tree
         if self.left:
@@ -38,7 +45,7 @@ class BinaryTreeNode:
             elements += self.right.in_order_traversal()
         return elements
     
-    def pre_order_traversal(self):
+    def pre_order_traversal(self): # base -> left -> right, also known as depth-first search (DFS)
         elements = []
         # visit base node
         elements.append(self.data)
@@ -50,56 +57,83 @@ class BinaryTreeNode:
             elements += self.right.pre_order_traversal()
         return elements
     
-    def build_tree(elements):
-        root = BinaryTreeNode(elements[0])
-        for i in range(1, len(elements)):
-            root.add_child(elements[i]) # adds elements to the tree by making them children of the root node, while maintaining the binary search tree property
-        
-        return root
-    
-    def post_order_traversal(self): 
+    def post_order_traversal(self): # left -> right -> base, also known as depth-first search (DFS)
         elements = []
         # visit left tree
         if self.left:
             elements += self.left.post_order_traversal()
         # visit right tree
         if self.right:
-            elements += self.right.post_order_traversal()
+            elements += self.right.post_order_traversal() 
         # visit base node
         elements.append(self.data)
 
         return elements
     def search(self, val):
-        found_at_level = 0
+        
         if self.data == val:
             return True
         if val < self.data: # search in left subtree
             if self.left:
                 return self.left.search(val)
-                found_at_level += 1
             else:
                 return False
             
         if val > self.data: # search in right subtree
             if self.right:
                 return self.right.search(val)
-                found_at_level += 1
             else:
                 return False
     
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+    
+    def delete(self, val): # big O(log n) time complexity because we are traversing only one path to find the element to be deleted
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else: # when the value to be deleted is found
+            # if the node to be deleted has no children, simply delete the node
+            if self.left is None and self.right is None:
+                return None
+            # if the node to be deleted has only one child, 
+            # return the child node to replace the node to be deleted
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.left
+            
+            # find the minimum element in the right subtree and replace the current node with that element, 
+            # then delete the minimum element in the right subtree until the base case is reached where the minimum element is deleted
+            min_val = self.right.find_min() 
+            self.data = min_val
+            self.right = self.right.delete(min_val) # big O(log n) time complexity because we are traversing the tree to find the minimum element in the right subtree
+
+        return self
+            
+
 
 if __name__ == '__main__':
     elements = [17, 4, 1, 20, 9, 23, 18, 34]
     elements_tree = BinaryTreeNode.build_tree(elements)
 
-    print(elements_tree.in_order_traversal()) # outputs [1, 4, 9, 17, 18, 20, 23, 34]
-    print(elements_tree.pre_order_traversal())  # outputs [17, 4, 1, 9, 20, 18, 23, 34]
-    print(elements_tree.post_order_traversal()) # outputs [1, 9, 4, 18, 34, 23, 20, 17]
+    print("In order traversal gives this sorted list:", elements_tree.in_order_traversal())
+    
+    # Testing delete method
+    elements_tree.delete(20)
+    print("In order traversal after deleting 20:", elements_tree.in_order_traversal())
 
-
-    print(elements_tree.search(20)) 
-    print(elements_tree.search(100)) # outputs False
-    print(elements_tree.search(17)) 
     
  
 
--- implementations/Data Structures/min_max_BST.py
+++ implementations/Data Structures/min_max_BST.py
@@ -53,6 +53,7 @@ if __name__ == '__main__':
     print("Maximum element in the tree is:", elements_tree.find_max())
 
 
+
  
 
     
