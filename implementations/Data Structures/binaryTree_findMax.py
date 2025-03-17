8599429b279146ba82c9cfc4df7b18a990935847
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-17 14:19:37 +0100

    finding max value of a BST with time complexity analysis

--- implementations/Data Structures/binaryTree_findMax.py
+++ implementations/Data Structures/binaryTree_findMax.py
@@ -15,92 +15,34 @@ class BinaryTreeNode:
         if data < self.data:
             # add data in left subtree
             if self.left:
-                self.left.add_child(data)
+                self.left.add_child(data) # big O(log n) time complexity because we are only traversing the left subtree of the tree
             else:
                 self.left = BinaryTreeNode(data)
             
         else:
             # add data in right subtree
             if self.right:
-                self.right.add_child(data)
+                self.right.add_child(data) # big O(log n) time complexity because we are only traversing the right subtree of the tree
             else:
                 self.right = BinaryTreeNode(data)
-
-    def in_order_traversal(self):
-        elements = []
-        # visit left tree
-        if self.left:
-            elements += self.left.in_order_traversal()
-        # visit base node
-        elements.append(self.data)
-        # visit right tree
-        if self.right:
-            elements += self.right.in_order_traversal()
-        return elements
-    
-    def pre_order_traversal(self):
-        elements = []
-        # visit base node
-        elements.append(self.data)
-        # visit left tree
-        if self.left:
-            elements += self.left.pre_order_traversal()
-        # visit right tree
-        if self.right:
-            elements += self.right.pre_order_traversal()
-        return elements
     
     def build_tree(elements):
         root = BinaryTreeNode(elements[0])
         for i in range(1, len(elements)):
-            root.add_child(elements[i]) # adds elements to the tree by making them children of the root node, while maintaining the binary search tree property
+            root.add_child(elements[i]) # big O(log n) time complexity because we are only traversing the left or right subtree of the tree, depending on the value of the element
         
         return root
     
-    def post_order_traversal(self): 
-        elements = []
-        # visit left tree
-        if self.left:
-            elements += self.left.post_order_traversal()
-        # visit right tree
-        if self.right:
-            elements += self.right.post_order_traversal()
-        # visit base node
-        elements.append(self.data)
-
-        return elements
-    def search(self, val):
-        found_at_level = 0
-        if self.data == val:
-            return True
-        if val < self.data: # search in left subtree
-            if self.left:
-                return self.left.search(val)
-                found_at_level += 1
-            else:
-                return False
-            
-        if val > self.data: # search in right subtree
-            if self.right:
-                return self.right.search(val)
-                found_at_level += 1
-            else:
-                return False
-    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max() # big O(log n) time complexity because we are only traversing the right subtree of the tree
 
 if __name__ == '__main__':
-    elements = [17, 4, 1, 20, 9, 23, 18, 34]
+    elements = [100, 17, 4, 1, 20, 9, 23, 18, 34, 90]
     elements_tree = BinaryTreeNode.build_tree(elements)
 
-    print(elements_tree.in_order_traversal()) # outputs [1, 4, 9, 17, 18, 20, 23, 34]
-    print(elements_tree.pre_order_traversal())  # outputs [17, 4, 1, 9, 20, 18, 23, 34]
-    print(elements_tree.post_order_traversal()) # outputs [1, 9, 4, 18, 34, 23, 20, 17]
-
-
-    print(elements_tree.search(20)) 
-    print(elements_tree.search(100)) # outputs False
-    print(elements_tree.search(17)) 
-    
+    print('The maximum is', elements_tree.find_max())
  
 
     
