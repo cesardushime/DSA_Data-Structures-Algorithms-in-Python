82ef6a02176c9f04cdd0334fffd0a598895eb524
Cesar Dushimimana <dushimezar2003@gmail.com>
2025-03-22 19:38:54 +0100

    added time complexity analysis of heapsort for minheap

--- implementations/Algorithms/heapSort.py
+++ implementations/Algorithms/heapSort.py
@@ -1,7 +1,11 @@
 import heapq
+# Time Complexity: O(nlogn)
+# heapq creates a min heap by default
+# heapq.heapify(arr) converts the list into a heap in O(n) time
+# heapq.heappop(arr) pops the smallest element from the heap in O(logn) time
 
 def heapsort(arr):
-    heapq.heapify(arr)
+    heapq.heapify(arr) 
     new_list = []
     for i in range(len(arr)):
         new_list.append(heapq.heappop(arr))
